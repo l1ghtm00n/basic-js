d7cefe22c5f96ad0948812ae1ed49c7daee3e00f
l1ghtm00n <lizardfrogdamn@gmail.com>
2022-10-12 13:54:47 +0300

    feat: made hanoi tower and transfrom array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let res = {turns : 0, seconds : 0};
+  res.turns = Math.pow(2,disksNumber) - 1;
+  res.seconds = Math.floor((3600/turnsSpeed) * res.turns);
+  return res;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  // let res = [];
+  if (Array.isArray(arr)) {
+    let l = arr.length;
+    let res = arr.slice();
+    for (let i=0; i<l; i++) {
+      if (arr[i] === commands[0]) {
+        if (i+1<l-1) {
+          res[i]=null;
+          res[i+1]=null;
+        } else {
+          res[i]=null;
+        }
+      } else if (arr[i] === commands[1]) {
+        if (i>0) {
+          res[i-1]=null
+          res[i]=null;
+        } else {
+          res[i]=null;
+        }
+      } else if (arr[i] === commands[2]) {
+        if (i!==l-1) {
+          res[i]=res[i+1];
+        } else {
+          res[i]=null;
+        }
+      } else if (arr[i] === commands[3]) {
+        if (i>0) {
+          res[i]=res[i-1];
+        } else {
+          res[i]=null;
+        }
+      } 
+    }
+    const results = [];
+    res.forEach(element => {
+      if (element !== null) {
+        results.push(element);
+      }
+    });
+    return results;
+  } else {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
 }
 
 module.exports = {
