822bc6f1b71001449ffd154af163328b503c5458
l1ghtm00n <lizardfrogdamn@google.com>
2022-10-13 22:34:36 +0300

    feat: laid foundation for mine sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let x = matrix.length;
+  let y = matrix[0].length;
+  let arr = new Array(x*y);
+  arr.fill(0,0);
+  for(let i=0; i< x; i++) {
+    for(let j=0; j< y; j++) {
+      if (matrix[i][j]) {
+        if (j+1<y) {
+          console.dir(`1) arr[${i},${j}] = ${i*x+j+1}`);
+          arr[i*x+j+1] += 1;
+        }
+        if (j-1>-1) {
+          console.dir(`2) arr[${i},${j}]  = ${i*x+j-1}`);
+          arr[i*x+j-1] += 1;
+        }
+        if (i+1<x) {
+          console.dir(`3) arr[${i},${j}]  = ${x+j*y+1}`);
+          arr[x*i+j*y+j] += 1;
+          if (x*i+j*y+x<x*y) {
+            arr[x*i+j*y+j+1] += 1;
+          }
+        }
+        if (i-1>-1) {
+          console.dir(`4) arr[${i},${j}]  = ${j*y-1}`);
+          arr[j*y-1] += 1;
+          if (j*y-x>-1) {
+            arr[j*y-x] += 1;
+          }
+        }    
+
+      }
+    }
+  }
+  console.dir(arr);
 }
 
 module.exports = {
